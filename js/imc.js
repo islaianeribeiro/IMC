4ccae4a64729780af7eb9c416ae847de8bedaeb2
Islaiane Ribeiro <islaiane.lribeiro@gmail.com>
2024-07-29 15:51:46 -0300

    mudando index

--- js/imc.js
+++ js/imc.js
@@ -2,8 +2,10 @@ function calculate() {
     var altura = document.getElementById("altura_input").value;
     var peso = document.getElementById("peso_input").value;
 
-    var imc = peso / altura ** 2
+    var imc = parseFloat((peso / (altura * altura)).toFixed(2));
 
-    document.querySelector('.calc-result strong').innerHTML = (imc).toFixed(2);
+    document.querySelector('.calc-result strong').innerHTML = (imc);
 
 }
+
+
