fc515d672c7135d429c3436fae19f317b43cc98a
Islaiane Ribeiro <islaiane.lribeiro@gmail.com>
2024-08-05 15:20:22 -0300

    update

--- js/imc.js
+++ js/imc.js
@@ -1,7 +1,7 @@
 // Função que calcula o IMC do usuário.
-function calcular() {
-    var altura = document.getElementById("altura_input").value;
-    var peso = document.getElementById("peso_input").value;
+function calculate() {
+    var altura = document.getElementById("height_input").value;
+    var peso = document.getElementById("weight_input").value;
 
     var imc = parseFloat((peso / (altura * altura)).toFixed(2));
 
@@ -10,28 +10,24 @@ function calcular() {
 }
 
 // Função que apaga o cálculo.
-function limpar() {
-    document.getElementById("altura_input").value = '';
-    document.getElementById("peso_input").value = '';
+function clean() {
+    document.getElementById("height_input").value = '';
+    document.getElementById("weight_input").value = '';
     document.querySelector('.calc-result strong').innerHTML = ('');
 }
 
 
 // Função para adicionar o '.' automaticamente nos input de altura e peso.
 function formatDecimalInput(event) {
-    // Obtém o valor atual do input
     let input = event.target;
     let value = input.value;
 
-    // Remove todos os caracteres não numéricos, exceto ponto
     value = value.replace(/[^0-9]/g, '');
 
-    // Adiciona o ponto decimal no lugar correto
     if (value.length > 2) {
         value = value.slice(0, -2) + '.' + value.slice(-2);
     }
 
-    // Atualiza o valor do input com a formatação
     input.value = value;
 }
 
